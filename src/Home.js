04be5ade26e8717585ce81f890d9a0069b0fa3e8
JVRPascua <pascuavincentjohn@gmail.com>
2021-10-01 18:34:58 +0800

    Learned about the useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 const Home = () => {
+    //let name = 'mario';
+    const [age, setAge] = useState(24);
+
+    const handleClick = () => {
+        setAge(age + 1);
+    }
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{age}</p>
+            <button onClick={handleClick}>Click Me!</button>
+
         </div>
      );
 }
